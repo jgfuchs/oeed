e78e3aee1d8447c65dbb37b0f6e2412e3e804b89
Julian Fuchs <jfuchs@mit.edu>
2018-02-08 17:12:33 -0500

    Minor refactoring

--- script.js
+++ script.js
@@ -20,37 +20,38 @@ var combining = {
   },
 }
 
-var inp = document.getElementById("inp");
+window.onload = function() {
+  var inp = document.getElementById("inp");
+  inp.putAtCursor = function(c, replace) {
+    var pos = this.selectionStart,
+        end = this.selectionEnd;
 
-var putAtCursor = function(c, replace) {
-  var pos = inp.selectionStart,
-      end = inp.selectionEnd;
-
-  if (replace) {
-    if (pos != end) {
-      return;
+    if (replace) {
+      if (pos != end) {
+        return;
+      }
+      pos -= 1;
     }
-    pos -= 1;
+
+    this.value = this.value.slice(0, pos) + c + this.value.slice(end);
+    this.setSelectionRange(pos+1, pos+1);
   }
 
-  inp.value = inp.value.slice(0, pos) + c + inp.value.slice(end);
-  inp.setSelectionRange(pos+1, pos+1);
-}
-
-inp.onkeydown = function(evt) {
-    if (evt.altKey) {
-      var k = evt.key;
-      if (k in letters) {
-        putAtCursor(letters[k], false);
-        return false;
-      }
-
-      if (k in combining) {
-        var prev = inp.value[inp.selectionStart - 1];
-        if (prev in combining[k]) {
-          putAtCursor(combining[k][prev], true);
+  inp.onkeydown = function(evt) {
+      if (evt.altKey) {
+        var k = evt.key;
+        if (k in letters) {
+          inp.putAtCursor(letters[k], false);
           return false;
         }
+
+        if (k in combining) {
+          var prev = inp.value[inp.selectionStart - 1];
+          if (prev in combining[k]) {
+            inp.putAtCursor(combining[k][prev], true);
+            return false;
+          }
+        }
       }
-    }
+  };
 };

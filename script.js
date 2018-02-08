d39b20c2314f62515a18d97f810926b05321b231
Julian Fuchs <jfuchs@mit.edu>
2018-02-08 17:28:52 -0500

    Autosave

--- script.js
+++ script.js
@@ -21,10 +21,16 @@ var combining = {
 }
 
 window.onload = function() {
-  var inp = document.getElementById("inp");
-  inp.putAtCursor = function(c, replace) {
-    var pos = this.selectionStart,
-        end = this.selectionEnd;
+  var inp = document.getElementById("inp"),
+      status = document.getElementById("status");
+
+  if (window.localStorage.text) {
+    inp.value = window.localStorage.text;
+  }
+
+  function putAtCursor(c, replace) {
+    var pos = inp.selectionStart,
+        end = inp.selectionEnd;
 
     if (replace) {
       if (pos != end) {
@@ -33,25 +39,36 @@ window.onload = function() {
       pos -= 1;
     }
 
-    this.value = this.value.slice(0, pos) + c + this.value.slice(end);
-    this.setSelectionRange(pos+1, pos+1);
+    inp.value = inp.value.slice(0, pos) + c + inp.value.slice(end);
+    inp.setSelectionRange(pos+1, pos+1);
   }
 
+  var tout = null;
+
   inp.onkeydown = function(evt) {
-      if (evt.altKey) {
-        var k = evt.key;
-        if (k in letters) {
-          inp.putAtCursor(letters[k], false);
+    if (tout) {
+      clearTimeout(tout);
+    }
+    tout = setTimeout(saveText, 250);
+
+    if (evt.altKey) {
+      var k = evt.key;
+      if (k in letters) {
+        putAtCursor(letters[k], false);
+        return false;
+      }
+
+      if (k in combining) {
+        var prev = inp.value[inp.selectionStart - 1];
+        if (prev in combining[k]) {
+          putAtCursor(combining[k][prev], true);
           return false;
         }
-
-        if (k in combining) {
-          var prev = inp.value[inp.selectionStart - 1];
-          if (prev in combining[k]) {
-            inp.putAtCursor(combining[k][prev], true);
-            return false;
-          }
-        }
       }
+    }
   };
+
+  function saveText() {
+    window.localStorage.text = inp.value;
+  }
 };
